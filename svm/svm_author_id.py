8ba012e13b00e1f2013b46cacc3e7a8ad8905966
Gar Mendiola <magahet@gmail.com>
2019-01-22 12:38:56 -0800

    finished SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,6 +28,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
 #features_train = features_train[:len(features_train)/100] 
 #labels_train = labels_train[:len(labels_train)/100] 
 
